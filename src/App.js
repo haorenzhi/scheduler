a82bed8d97c735622a752ac0f722eaad2bca0213
Horange <horange1024@outlook.com>
2022-01-17 23:56:48 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const schedule = {
 };
 
 const App = () => (
-  <div>
+  <div className="container">
     <Banner title={schedule.title}/>
     <CourseList courses={schedule.courses}/>
   </div>
@@ -41,7 +41,7 @@ const Banner = ({title}) => (
 )
 
 const CourseList = ({courses}) => (
-  <div>
+  <div className="course-list">
     {Object.values(courses).map(course =><Course key={course.id} course={course}/>)}
   </div>
 )
@@ -52,9 +52,12 @@ const getCourseTerm= course =>(terms[course.id.charAt(0)])
 
 const getCourseNumber= course => (course.id.slice(1,4))
 
-const Course = ({course}) => (
-  <div>
-    {getCourseTerm(course)} CS {getCourseNumber(course)}: {course.title}
+const Course = ({course}) =>(
+  <div className="card m-1 p-2">
+    <dive className="card-body">
+      <div className="card-title">{ getCourseTerm(course)} CS {getCourseNumber(course)}</div>
+      <div className="card-text">{ course.title }</div>
+    </dive>
   </div>
 )
 
